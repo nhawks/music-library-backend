29001d7c70fc7f749cb13b969ffc93a36ea6e328
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-01 18:39:10 -0400

    Update views.py
    Delete function added.

--- musiclibrary/music/views.py
+++ musiclibrary/music/views.py
@@ -47,4 +47,9 @@ class SongDetail(APIView):
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST,
         )
+    
+    def delete(self, request, pk):
+        song = self.get_object(pk)
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
         
