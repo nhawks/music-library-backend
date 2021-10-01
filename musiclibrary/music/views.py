61ab582532145c6831f89c32f7ca94ac5839b1d5
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-01 19:32:19 -0400

    Updated status responses

--- musiclibrary/music/views.py
+++ musiclibrary/music/views.py
@@ -2,7 +2,7 @@ from .models import Song
 from .serializers import SongSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import serializers, status
 from django.http import Http404
 
 # Create your views here.
@@ -10,7 +10,10 @@ class SongList(APIView):
     def get(self, request):
         song = Song.objects.all()
         serializer = SongSerializer(song, many=True)
-        return Response(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
     
     def post(self, request):
         serializer = SongSerializer(data=request.data)
@@ -18,7 +21,7 @@ class SongList(APIView):
             serializer.save()
             return Response(
                 serializer.data, 
-                status=status.HTTP_202_ACCEPTED,
+                status=status.HTTP_201_CREATED,
             )
         return Response(
             serializer.errors, 
@@ -35,7 +38,10 @@ class SongDetail(APIView):
     def get(self, request, pk):
         song = self.get_object(pk)
         serializer = SongSerializer(song)
-        return Response(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
     
     def put(self, request, pk):
         song = self.get_object(pk)
@@ -45,11 +51,15 @@ class SongDetail(APIView):
             return Response(serializer.data)
         return Response(
             serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST,
+            status=status.HTTP_200_OK,
         )
     
     def delete(self, request, pk):
         song = self.get_object(pk)
+        serializer = SongSerializer(song)
         song.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(
+            serializer.data, 
+            status=status.HTTP_200_OK
+        )
         
