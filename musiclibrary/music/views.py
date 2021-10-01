f8b67cadc07f573c1da9689ef71938d700d04578
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-01 14:06:41 -0400

    Updated views.py
    - Added imports needed for Class-based views
    - created SongList view

--- musiclibrary/music/views.py
+++ musiclibrary/music/views.py
@@ -1,3 +1,15 @@
+import re
 from django.shortcuts import render
+from .models import Song
+from .serializers import SongSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers, status
+
 
 # Create your views here.
+class SongList(APIView):
+    def get(self, request):
+        song = Song.objects.all()
+        serializer = SongSerializer(song, many=True)
+        return Response(serializer.data)
